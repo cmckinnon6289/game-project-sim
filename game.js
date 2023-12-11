45017ac8ec2a7ee654693c702d6b072190d6f444
cmckinnon6289 <cmckinnon@bayviewglen.ca>
2023-12-11 13:40:58 -0500

    first commit

---
+++ game.js
@@ -0,0 +1,41 @@
+function simulate(trials) {
+    const A = 1
+    const J = 11
+    const Q = 12
+    const K = 13
+    const cards = [A,2,3,4,5,6,7,8,9,10,J,Q,K];
+    
+    for (i=0; i<trials; i++) {
+        let die = Math.round(Math.random()*6) + 1
+        if (die === 7) die -= 1;
+        
+        let card1 = Math.round(Math.random()*cards.length)-1;
+        let card2 = die%2 === 0 ? Math.round(Math.random()*cards.length)-1 : 0;
+
+        let total = (card1 + card2) - die;
+        display(i,die,card1,card2,total);
+    }
+}
+
+function display(i,d,c1,c2,t) {
+    let trial = i;
+    let die = d;
+    let card1 = c1;
+    let card2 = c2 === 0 ? "N/A" : c2;
+    let total = t;
+
+    let row = document.createElement("tr");
+    let trialDOM = document.createElement("td");
+    let dieDOM = document.createElement("td");
+    let card1DOM = document.createElement("td");
+    let card2DOM = document.createElement("td");
+    let totalDOM = document.createElement("td");
+
+    trialDOM.textContent
+
+    row.appendChild(trialDOM);
+    row.appendChild(dieDOM);
+    row.appendChild(card1DOM);
+    row.appendChild(card2DOM);
+    row.appendChild(totalDOM);
+}
